2d024ca6a5ce1a4b1097c25572ac76b80ae48934
CptPow <capitanpow@gmail.com>
2023-08-02 20:38:18 +0200

    diary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import ShowPost from "./ShowPost";
 
 function App() {
+  const [form, setForm] = useState({
+    titolo: "",
+    data: "",
+    testo: "",
+    
+  });
+
+  const [post, setPost] = useState([]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setForm({
+      ...form,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (form.titolo && form.data && form.testo) {
+      setPost([...post, { ...form }]);
+      showData();
+      setForm({
+        titolo: "",
+        data: "",
+        testo: "",
+      });
+      <ShowPost titolo={titolo} />
+    } else {
+      alert("Riempi i campi vuoti...");
+    }
+  };
+
+  const showData = ()=> {
+    console.log(`Titolo: ${form.titolo}`);
+    console.log(`Data: ${form.data}`);
+    console.log(`Testo: ${form.testo}`);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form className="bg-secondary container rounded p-3">
+      <div className="form-group d-flex align-items-center justify-content-center col-6 ms-5 p-2">
+        {/*TITOLO*/}
+        <label className="fw-bold me-3" htmlFor="titolo">
+          Titolo:
+        </label>
+        <input
+          onChange={handleChange}
+          value={form.titolo}
+          id="titolo"
+          type="text"
+          name="titolo"
+          className="form-control col-8"
+        ></input>
+      </div>
+      {/*DATA*/}
+      <div className="form-group d-flex align-items-center justify-content-center col-6 ms-5 p-2">
+        <label className="fw-bold me-3" htmlFor="data">
+          Data:
+        </label>
+        <input
+          onChange={handleChange}
+          value={form.data}
+          id="data"
+          type="date"
+          name="data"
+          className="form-control col-8"
+        ></input>
+      </div>
+      {/*TEXTAREA*/}
+      <div className="form-group d-flex align-items-center justify-content-center col-6 ms-5 p-2">
+        <label className="fw-bold me-3" htmlFor="testo">
+          Testo:
+        </label>
+        <textarea
+          onChange={handleChange}
+          id="testo"
+          name="testo"
+          value={form.testo}
+        ></textarea>
+      </div>
+      {/*BUTTON*/}
+      <div className="form-group d-flex align-items-center justify-content-center col-6 ms-5 p-2">
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="rounded-5 btn btn-primary col-6"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Aggiungi pagina
+        </button>
+      </div>
+    </form>
+    
   );
 }
 
